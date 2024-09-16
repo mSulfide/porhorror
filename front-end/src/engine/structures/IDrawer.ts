3a268523c61c8ab88b64d9d2967f282d84ec07cc
Sulfide <msulfide@mail.ru>
2024-09-16 17:33:38 +0300

    add comment

--- front-end/src/engine/structures/IDrawer.ts
+++ front-end/src/engine/structures/IDrawer.ts
@@ -1,5 +1,6 @@
 import { Point } from ".";
 
 export default interface IDrawer {
+    //рисует картинку в указанной позиции. Позиция передаётся в игровых координатах. Сама картинка пока отсутствет в параметрах
     drawSprite(position: Point): void;
 }
