d794a2023e514d4ffedfd95022891da79ebeca3d
Sulfide <okey.kristina@mail.ru>
2024-09-16 14:17:24 +0300

    Add '123'

--- front-end/src/engine/entities/MegaDrawer.ts
+++ front-end/src/engine/entities/MegaDrawer.ts
@@ -3,6 +3,6 @@ import IDrawer from "../structures/IDrawer";
 
 export default class MegaDrawer implements IDrawer {
     drawSprite(position: Point): void {
-        console.log(position);
+        console.log(position, "123");
     }
 }
