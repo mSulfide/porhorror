9c5b7725067dfa0aaad4c39a789300ad83d6eb5c
poyasonya <sofya.konysheva.2005@gmail.com>
2024-10-01 22:06:29 +0400

    Update Game.tsx
    Убрала комментарии

--- front-end/src/engine/Game.tsx
+++ front-end/src/engine/Game.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useRef } from 'react'; //добавлен useRef
+import { useEffect, useRef } from 'react'; 
 import Scene from './structures/Scene';
 import MainScreen from './structures/MainScreen';
 import Player from './entities/objects/Player';
 import KeyboardInput from './structures/KeyboardInput';
 import IInput from './interfaces/IInput';
-import CanvasDrawer from './structures/CanvasDrawer'; //добавлено
+import CanvasDrawer from './structures/CanvasDrawer'; 
 
 const Game: React.FC = () => {
 
-  const canvasRef = useRef<HTMLCanvasElement>(null); //добвлено
+  const canvasRef = useRef<HTMLCanvasElement>(null); 
 
   const input: IInput = new KeyboardInput();
   const scene: Scene = new Scene([new Player(input)]);
@@ -25,9 +25,9 @@ const Game: React.FC = () => {
     let dTimestamp = Date.now();
     let deltaTime = 0;
 
-    const canvas = canvasRef.current!; //добавлено
-    const drawer = new CanvasDrawer(canvas); //добавлено 
-    const screen = new MainScreen(drawer); //изменено
+    const canvas = canvasRef.current!; 
+    const drawer = new CanvasDrawer(canvas); 
+    const screen = new MainScreen(drawer); 
 
     const loop = () => {
       const currentTimestamp = Date.now();
@@ -47,65 +47,14 @@ const Game: React.FC = () => {
     return () => {
       window.cancelAnimationFrame(idLoop);
     };
-  }, []); //изменено
+  }, []); 
 
   return (
     <>
       <canvas ref={canvasRef} width={400} height={300} /> 
-    </> //добавлено
+    </>
   );
 };
 
 export default Game;
 
-
-/*import { useEffect } from "react";
-import Scene from "./structures/Scene";
-import ConsoleDrawer from "./structures/ConsoleDrawer";
-import MainScreen from "./structures/MainScreen";
-import Player from "./entities/objects/Player";
-import KeyboardInput from "./structures/KeyboardInput";
-import IInput from "./interfaces/IInput";
-
-const Game: React.FC = () => {
-    const input: IInput = new KeyboardInput;
-    const scene: Scene = new Scene([new Player(input)]);
-
-    const update = (deltaTime: number) => {
-        scene.forEach(gameObject => {
-            gameObject.update(deltaTime);
-        });
-    };
-
-    useEffect(() => {
-        let idLoop: number;
-
-        let dTimestamp = Date.now();
-        let deltaTime = 0;
-
-        const screen: MainScreen = new MainScreen(new ConsoleDrawer());
-
-        const loop = () => {
-            const currentTimestamp = Date.now();
-
-            deltaTime = (currentTimestamp - dTimestamp) / 1000;
-            dTimestamp = currentTimestamp;
-
-            update(deltaTime);
-
-            screen.render(scene);
-
-            idLoop = window.requestAnimationFrame(loop);
-        }
-
-        loop();
-
-        return () => {
-            window.cancelAnimationFrame(idLoop);
-        }
-    });
-
-    return <></>;
-};
-
-export default Game;*/
