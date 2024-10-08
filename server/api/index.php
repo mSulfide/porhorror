8db72ba468b208eec4a3b165bcbbe8ffd071a5ff
Sulfide <msulfide@mail.ru>
2024-10-08 15:51:01 +0300

    add server

---
+++ server/api/index.php
@@ -0,0 +1,29 @@
+<?php
+
+error_reporting(1);
+
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+require_once('application/Answer.php');
+require_once('application/Application.php');
+
+function result($params) {
+    $method = $params['method'];
+    if ($method) {
+        $app = new Application();
+        switch ($method) {
+            // user
+            case 'login': return $app->login($params);
+            case 'logout': return $app->logout($params);
+            case 'registration': return $app->registration($params);
+            // chat
+            case 'sendMessage': return $app->sendMessage($params);
+            case 'getMessages': return $app->getMessages($params);
+            default: return ['error' => 102];
+        }
+    }
+    return ['error' => 101];
+}
+
+echo json_encode(Answer::response(result($_GET)), JSON_UNESCAPED_UNICODE);
