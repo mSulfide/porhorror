bdaeb32ed6c28826448a6bebf0e4ca73e76fe181
A-Shto-Govorit <son.miyagi69@gmail.com>
2024-10-12 23:45:41 +0400

    Update Math.php

--- server/api/application/Math/Math.php
+++ server/api/application/Math/Math.php
@@ -10,36 +10,51 @@ function der($func, $x, $eps) {
 }
 
 function derivative($func, $x, $eps) {
-    return array (
-        'result' => 'ok',
-        'data' => der($func, $x, $eps)
-    );
+    return der($func, $x, $eps)
 }
 //сплайны
-function spline($x1, $y1, $x2, $y2, $x3, $y3) {
-    $h1 = $x2 - $x1;
-    $h2 = $x3 - $x2;
-    
-    $a1 = ($y2 - $y1) / $h1;
-    $a2 = ($y3 - $y2) / $h2;
-    
-    $b1 = 3 * (($y2 - $y1) / ($h1 * $h1)) - 2 * (($y3 - $y1) / ($h1 * $h2)) + (($y3 - $y2) / ($h2 * $h2));
-    $b2 = 3 * (($y3 - $y2) / ($h2 * $h2)) - 2 * (($y3 - $y1) / ($h1 * $h2)) + (($y2 - $y1) / ($h1 * $h1));
-    
-    $c1 = (($y3 - $y1) / ($h1 * $h2)) - (($y2 - $y1) / ($h1 * $h1)) - $b1 * $h1 / 3;
-    $c2 = (($y2 - $y1) / ($h1 * $h2)) - (($y3 - $y2) / ($h2 * $h2)) - $b2 * $h2 / 3;
-    
-    $splinePoints = [];
-    $x = $x1;
-    while ($x <= $x3) {
-        if ($x <= $x2) {
-            $y = $y1 + $a1 * ($x - $x1) + $b1 * pow($x - $x1, 2) / 2 + $c1 * pow($x - $x1, 3) / 6;
-        } else {
-            $y = $y2 + $a2 * ($x - $x2) + $b2 * pow($x - $x2, 2) / 2 + $c2 * pow($x - $x2, 3) / 6;
-        }
-        $splinePoints[] = ['x' => $x, 'y' => $y];
-        $x += 0.01; //шаг для отрисовки графика
+function spline(array $points) {
+    $n = count($points);
+    if ($n < 3) {
+        throw new InvalidArgumentException("Необходимо минимум 3 точки для сплайна.");
     }
-return $splinePoints
-        
+
+    $coefficients = [];
+    for ($i = 0; $i < $n - 1; $i++) {
+        $x1 = $points[$i]['x'];
+        $y1 = $points[$i]['y'];
+        $x2 = $points[$i + 1]['x'];
+        $y2 = $points[$i + 1]['y'];
+
+        if ($i < $n - 2) {
+            $x3 = $points[$i + 2]['x'];
+            $y3 = $points[$i + 2]['y'];
+        } else {
+            $x3 = $x2;
+            $y3 = $y2; //  Для последней секции используем последнюю точку
+        }
+
+        $h1 = $x2 - $x1;
+        $h2 = $x3 - $x2;
+
+        $a1 = ($y2 - $y1) / $h1;
+        $a2 = ($y3 - $y2) / $h2;
+
+        $b1 = 3 * (($y2 - $y1) / ($h1 * $h1)) - 2 * (($y3 - $y1) / ($h1 * $h2)) + (($y3 - $y2) / ($h2 * $h2));
+        $b2 = 3 * (($y3 - $y2) / ($h2 * $h2)) - 2 * (($y3 - $y1) / ($h1 * $h2)) + (($y2 - $y1) / ($h1 * $h1));
+
+        $c1 = (($y3 - $y1) / ($h1 * $h2)) - (($y2 - $y1) / ($h1 * $h1)) - $b1 * $h1 / 3;
+        $c2 = (($y2 - $y1) / ($h1 * $h2)) - (($y3 - $y2) / ($h2 * $h2)) - $b2 * $h2 / 3;
+
+        $coefficients[] = [
+            'a' => $a1,
+            'b' => $b1,
+            'c' => $c1,
+            'd' => $y1,
+            'x_start' => $x1,
+            'x_end' => $x2,
+        ];
+    }
+
+    return $coefficients;
 }
