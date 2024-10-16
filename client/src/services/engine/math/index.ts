30095cfba1e98d5d2f5fa33fe79ea3e1cdb62d82
Sulfide <msulfide@mail.ru>
2024-10-17 01:44:59 +0300

    add Math

---
+++ client/src/services/engine/math/index.ts
@@ -0,0 +1,17 @@
+import { TPoint } from ".."
+
+const add = (a: TPoint, b: TPoint): TPoint => ({ x: a.x + b.x, y: a.y + b.y });
+
+const sub = (a: TPoint, b: TPoint): TPoint => ({ x: a.x - b.x, y: a.y - b.y });
+
+const mlt = (a: TPoint, p: number): TPoint => ({ x: a.x * p, y: a.y * p })
+
+const dot = (a: TPoint, b: TPoint): number => a.x * b.x + a.y * b.y;
+
+const smod = (a: TPoint): number => dot(a, a);
+
+const modl = (a: TPoint): number => Math.sqrt(smod(a));
+
+const norm = (a: TPoint): TPoint => mlt(a, 1 / modl(a));
+
+export { add, sub, mlt, dot, smod, modl, norm }
