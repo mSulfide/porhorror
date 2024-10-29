ea9c3410aaec7eafe6520c6e7826cbb83098082d
Sulfide <msulfide@mail.ru>
2024-10-30 00:02:53 +0300

    remove Vector

--- client/src/services/engine/structures/index.ts
+++ client/src/services/engine/structures/index.ts
@@ -1,6 +1,5 @@
 import Input from "./Input/Input";
 import Physic from "./Physic/Physic";
 import Scene from "./Scene/Scene";
-import Vector from "./Vector/Vector";
 
-export { Scene, Vector, Physic, Input };
+export { Scene, Physic, Input };
