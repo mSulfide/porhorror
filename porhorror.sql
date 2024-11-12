e0791b9e8e092b0c3977c0be96f871875e65c0e2
Sulfide <msulfide@mail.ru>
2024-11-13 02:10:50 +0300

    DB
    Простите дурака. Забыл экспортировать БД
    
    Ну, как офигевший тимлид могу позволить себе пушить в develop
    ыыы

--- porhorror.sql
+++ porhorror.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 06 2024 г., 14:48
+-- Время создания: Ноя 13 2024 г., 02:08
 -- Версия сервера: 8.0.30
 -- Версия PHP: 7.2.34
 
@@ -38,6 +38,26 @@ CREATE TABLE `hashes` (
 
 INSERT INTO `hashes` (`id`, `chat_hash`) VALUES
 (1, '965cad4883471d9ed7a8f0ffc20cbd25');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lobby`
+--
+
+CREATE TABLE `lobby` (
+  `id` int NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `is_started` tinyint(1) NOT NULL DEFAULT '0',
+  `creator_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `lobby`
+--
+
+INSERT INTO `lobby` (`id`, `name`, `is_started`, `creator_id`) VALUES
+(1, 'МегаЛобби-1', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -82,9 +102,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `name`, `token`) VALUES
-(1, 'sulfide', '6f1f3d80cbb51102cf626135afbae1aa', 'Миша', '4cb359b0f1c1bbcf9956697d34bba8ec'),
+(1, 'sulfide', '6f1f3d80cbb51102cf626135afbae1aa', 'Миша', '4a4be00d0a1317f13e0e95f81b469f4a'),
 (2, 'vasya', 'fcb03559c0317682f5d65a88aca50012', 'Вася', '6c1a35c84af6b2594544fdc5c2f52f0f'),
-(3, 'petya', 'd7ba312b012b3374ef53eb2e3f9830a5', 'Петя', 'cc79d5f20b41d4728ae7eb7157cde2a0');
+(3, 'petya', 'd7ba312b012b3374ef53eb2e3f9830a5', 'Петя', 'cc79d5f20b41d4728ae7eb7157cde2a0'),
+(4, 'brandon', 'b015f4f164ef51727ff751635f7d0eaf', 'Барсук', 'a2fa8c76f744110ef9182ff97b1255c1');
 
 --
 -- Индексы сохранённых таблиц
@@ -94,6 +115,12 @@ INSERT INTO `users` (`id`, `login`, `password`, `name`, `token`) VALUES
 -- Индексы таблицы `hashes`
 --
 ALTER TABLE `hashes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `lobby`
+--
+ALTER TABLE `lobby`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -119,6 +146,12 @@ ALTER TABLE `hashes`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT для таблицы `lobby`
+--
+ALTER TABLE `lobby`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT для таблицы `messages`
 --
 ALTER TABLE `messages`
@@ -128,7 +161,7 @@ ALTER TABLE `messages`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
