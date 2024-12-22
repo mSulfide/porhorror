b7d5153b17e7e2fa4e05a32d7bab93ca799cebe9
kon1711 <42605885+kon12@users.noreply.github.com>
2024-12-23 03:58:45 +0400

    deleteLot
    снятия лота

--- server/api/application/exchanger/Exchanger.php
+++ server/api/application/exchanger/Exchanger.php
@@ -25,4 +25,18 @@ class Exchanger {
         }
         return ['error'=> '809'];
     }
+
+    public function deleteLot($lotId, $userId) {
+        // Получить информацию о лоте
+        $lot = $this->db->getLotById($lotId);
+        if ($lot) {
+            // Проверяем, принадлежит ли лот текущему пользователю
+            if ($lot->ownerId === $userId) {
+                $this->db->deleteLot($lotId); // Удаляем лот
+                return ['success' => true];
+            }
+            return ['error' => 1008]; // Лот не принадлежит пользователю
+        }
+        return ['error' => 1007]; // Лот не найден
+    }
 }
