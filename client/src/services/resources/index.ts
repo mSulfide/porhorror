0d08344ea11ce8f7a014d6b12aeeaef51193ca42
Sulfide <msulfide@mail.ru>
2024-12-24 17:12:47 +0300

    add tas1

--- client/src/services/resources/index.ts
+++ client/src/services/resources/index.ts
@@ -1,9 +1,12 @@
 import tas from "../../assets/img/tas.png";
+import tas1 from "../../assets/img/authors/alex_pixel-art.png";
 
 export enum EImage {
-    tas = 'tas'
+    tas = 'tas',
+    tas1 = 'tas1'
 }
 
 export const srcs = {
-    [EImage.tas]: tas
+    [EImage.tas]: tas,
+    [EImage.tas1]: tas1
 }
