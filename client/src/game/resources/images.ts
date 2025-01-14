95656bee6c61b13f5e37e182a527c4cd4a9d6b94
uselence <uselence@gmail.com>
2025-01-14 13:12:00 +0400

    sprites porting
    textures files updated, sprites imported

--- client/src/game/resources/images.ts
+++ client/src/game/resources/images.ts
@@ -1,12 +1,21 @@
 import tas from "../../assets/img/tas.png";
-import assets from "../../assets/img/alpha/assets0.png"
+import environment from "../../assets/img/alpha/environment.png"
+import items from "../../assets/img/alpha/items.png"
+import patterns from "../../assets/img/alpha/patterns.png"
+import walls from "../../assets/img/alpha/walls.png"
 
 export enum EImage {
     tas = 'tas',
-    assets = 'assets'
+    environment = 'environment',
+    items = 'items',
+    patterns = 'patterns',
+    walls = 'walls'
 }
 
 export const srcs = {
     [EImage.tas]: tas,
-    [EImage.assets]: assets
+    [EImage.environment]: environment,
+    [EImage.items]: environment,
+    [EImage.patterns]: environment,
+    [EImage.walls]: environment
 }
--- client/src/game/resources/sprites.ts
+++ client/src/game/resources/sprites.ts
@@ -2,7 +2,44 @@ import { EImage } from "./images"
 
 export enum ESprite {
     tas = 'tas',
-    player = 'player'
+    // ENVIRONMENT
+    wardrobeENV = 'wardrobeENV',
+    tableRightENV = 'tableRightENV',
+    tableLeftENV = 'tableLeftENV',
+    tableBothENV = 'tableBothENV',
+    tableEmptyENV = 'tableEmptyENV',
+    wateringCanENV = 'wateringCanENV',
+    cookiesENV = 'cookiesENV',
+    xeroxENV = 'xeroxENV',
+    printerENV = 'printerENV',
+    blackCoffieMachineENV = 'blackCoffieMachineENV',
+    redCoffieMachineENV = 'redCoffieMachineENV',
+    trashCanENV = 'trashCanENV',
+    potPlantENV = 'potPlantENV',
+    dryPotPlantENV = 'dryPotPlantENV',
+    toiletENV = 'toiletENV',
+    // ITEMS
+    cookieITM = 'cookieITM',
+    horizPhotoITM = 'horizPhotoITM',
+    verticPhotoITM = 'verticPhotoITM',
+    coffeeITM = 'coffeeITM',
+    wateringCanITM = 'wateringCanITM',
+    paperITM = 'paperITM',
+    gradeBookITM = 'gradeBookITM',
+    // PATTERNS
+    linoleumPAT = 'linoleumPAT',
+    slabsPAT = 'slabsPAT',
+    // WALLS - walls
+    allSidesWLL = 'allSidesWLL',
+    longSidesWLL = 'longSidesWLL',
+    threeSidesWLL = 'threeSidesWLL',
+    longSideWLL = 'longSideWLL',
+    cornerWLL = 'cornerWLL',
+    // WALLS - columns
+    allSidesColumnWLL = 'allSidesColumnWLL',
+    cornerColumnWLL = 'cornerColumnWLL',
+    threeSidesColumnWLL = 'threeSidesColumnWLL'
+
 }
 
 export const getSprite = (image: EImage, offset: number[], size: number[]) => ({
@@ -12,6 +49,42 @@ export const getSprite = (image: EImage, offset: number[], size: number[]) => ({
 });
 
 export const sprites = {
+    // ENVIRONMENT
     [ESprite.tas]: getSprite(EImage.tas, [0, 0], [32, 32]),
-    [ESprite.player]: getSprite(EImage.assets, [101, 6], [21, 21])
+    [ESprite.wardrobeENV]: getSprite(EImage.environment, [0, 0], [32, 32]),
+    [ESprite.tableRightENV]: getSprite(EImage.environment, [32, 0], [64, 32]),
+    [ESprite.tableLeftENV]: getSprite(EImage.environment, [96, 0], [64, 32]),
+    [ESprite.tableBothENV]: getSprite(EImage.environment, [32, 32], [64, 32]),
+    [ESprite.tableEmptyENV]: getSprite(EImage.environment, [96, 32], [64, 32]),
+    [ESprite.wateringCanENV]: getSprite(EImage.environment, [0, 32], [32, 32]),
+    [ESprite.cookiesENV]: getSprite(EImage.environment, [0, 64], [32, 32]),
+    [ESprite.xeroxENV]: getSprite(EImage.environment, [32, 64], [32, 32]),
+    [ESprite.printerENV]: getSprite(EImage.environment, [64, 64], [32, 32]),
+    [ESprite.blackCoffieMachineENV]: getSprite(EImage.environment, [96, 64], [32, 32]),
+    [ESprite.redCoffieMachineENV]: getSprite(EImage.environment, [128, 64], [32, 32]),
+    [ESprite.trashCanENV]: getSprite(EImage.environment, [0, 96], [32, 32]),
+    [ESprite.potPlantENV]: getSprite(EImage.environment, [32, 96], [32, 32]),
+    [ESprite.dryPotPlantENV]: getSprite(EImage.environment, [64, 96], [32, 32]),
+    [ESprite.toiletENV]: getSprite(EImage.environment, [96, 96], [32, 32]),
+    // ITEMS
+    [ESprite.horizPhotoITM]: getSprite(EImage.environment, [0, 0], [16, 16]),
+    [ESprite.verticPhotoITM]: getSprite(EImage.environment, [16, 0], [16, 16]),
+    [ESprite.coffeeITM]: getSprite(EImage.environment, [32, 0], [16, 16]),
+    [ESprite.wateringCanITM]: getSprite(EImage.environment, [48, 0], [16, 16]),
+    [ESprite.paperITM]: getSprite(EImage.environment, [64, 0], [16, 16]),
+    [ESprite.cookieITM]: getSprite(EImage.environment, [0, 16], [16, 16]),
+    [ESprite.gradeBookITM]: getSprite(EImage.environment, [16, 16], [16, 16]),
+    // PATTERNS
+    [ESprite.linoleumPAT]: getSprite(EImage.environment, [0, 0], [32, 32]),
+    [ESprite.slabsPAT]: getSprite(EImage.environment, [32, 0], [32, 32]),
+    // WALLS - walls
+    [ESprite.allSidesWLL]: getSprite(EImage.environment, [0, 0], [8, 32]),
+    [ESprite.longSidesWLL]: getSprite(EImage.environment, [8, 0], [8, 32]),
+    [ESprite.threeSidesWLL]: getSprite(EImage.environment, [16, 0], [8, 32]),
+    [ESprite.longSideWLL]: getSprite(EImage.environment, [24, 0], [8, 32]),
+    [ESprite.cornerWLL]: getSprite(EImage.environment, [32, 0], [8, 32]),
+    // WALLS - columns
+    [ESprite.allSidesColumnWLL]: getSprite(EImage.environment, [0, 0], [32, 32]),
+    [ESprite.cornerColumnWLL]: getSprite(EImage.environment, [32, 32], [32, 32]),
+    [ESprite.threeSidesColumnWLL]: getSprite(EImage.environment, [0, 64], [32, 32]),
 }
