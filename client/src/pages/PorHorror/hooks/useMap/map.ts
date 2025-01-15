c59aa4688951a06d6207d4b637a2fcbe994e9d48
Sulfide <msulfide@mail.ru>
2025-01-15 21:17:44 +0300

    add fill

--- client/src/pages/PorHorror/hooks/useMap/map.ts
+++ client/src/pages/PorHorror/hooks/useMap/map.ts
@@ -1,13 +1,18 @@
 import { ESprite } from "../../../../game/resources";
+import Tools from "./Tools/Tools";
 type TPoint = { x: number, y: number };
-type TObject = {
+export type TMapObject = {
     sprite: ESprite,
     position: TPoint,
     size: TPoint
 };
 
-const getMap = (): TObject[] => {
-    const map: TObject[] = [];
+const getMap = (): TMapObject[] => {
+    const map: TMapObject[] = [];
+    const tools = new Tools(map);
+
+    tools.fill(1, 1, 8, 5, ESprite.toiletENV, 1, 1);
+
     return map;
 };
 
