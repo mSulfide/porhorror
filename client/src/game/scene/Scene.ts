b471b2d041d59cd7bd509f09ce596cee2108735b
Sulfide <msulfide@mail.ru>
2025-01-17 01:43:29 +0300

    smoothing by acceleration

--- client/src/game/scene/Scene.ts
+++ client/src/game/scene/Scene.ts
@@ -1,15 +1,24 @@
+import { TPoint, mlt, sub } from "../../services/math";
 import { TGameObject } from "../../services/server/types";
 
-type TSceneObject = Omit<TGameObject, 'id'>;
+type TSceneObject = Omit<TGameObject, 'id'> & {
+    acceleration?: TPoint,
+    velocityA?: TPoint;
+};
 
 class Scene {
     private scene: { [id: string]: TSceneObject } = {};
+    private lastSetTime: number = 0;
 
     set(scene: TGameObject[]) {
         scene.forEach(({ id, image, position, velocity, radius, angle }) => {
             const gameObject = this.scene[id];
             if (gameObject) {
                 gameObject.image = image;
+                if (gameObject.velocityA) {
+                    gameObject.acceleration = mlt(sub(velocity, gameObject.velocityA), 1 / this.lastSetTime);
+                }
+                gameObject.velocityA = { x: velocity.x, y: velocity.y };
                 gameObject.position = position;
                 gameObject.velocity = velocity;
                 gameObject.radius = radius;
@@ -19,9 +28,11 @@ class Scene {
                 this.scene[id] = { image, position, velocity, radius, angle };
             }
         });
+        this.lastSetTime = 0;
     }
 
     update(deltaTime: number): void {
+        this.lastSetTime += deltaTime;
         // GAME_TIMESTAMP: 1000, //ms
         // FPS - частота отрисовки экрана
         // TIME_REQUEST - время ответа сервера
@@ -34,9 +45,13 @@ class Scene {
         0_____|_____|_____|_____|_____|_____|____1000 + TIME_REQUEST
         */
         Object.keys(this.scene).forEach((id) => {
-            const transform = this.scene[id];
-            transform.position.x = transform.position.x + transform.velocity.x * deltaTime;
-            transform.position.y = transform.position.y + transform.velocity.y * deltaTime;
+            const { position, velocity, acceleration } = this.scene[id];
+            position.x = position.x + velocity.x * deltaTime;
+            position.y = position.y + velocity.y * deltaTime;
+            if (acceleration) {
+                velocity.x = velocity.x + acceleration.x * deltaTime;
+                velocity.y = velocity.y + acceleration.y * deltaTime;
+            }
         });
     }
 }
